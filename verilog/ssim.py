0027d3455fbb8b2aade97afcc380e625b5ad4a44
obijuan <juan@iearobotics.com>
2015-12-04 08:39:53 +0100

    [ssim.py] Code refactored. Implemented the BR and BZ instructions

--- verilog/ssim.py
+++ verilog/ssim.py
@@ -1,4 +1,4 @@
-import sasm
+# import sasm
 
 
 class Test_progs(object):
@@ -32,7 +32,7 @@ class simplez(object):
         self._A = 0   # - A register
         self._PC = 0  # - Program counter
         self._Z = 0   # - Z flag
-        self.mem = [0 for i in range(512)]  # - Simplez memory
+        self._mem = [0 for i in range(512)]  # - Simplez memory
         self.state = self.INIT
 
     def decode(self, inst):
@@ -45,26 +45,64 @@ class simplez(object):
         print("Z = {}".format(self._Z))
         if self.state == self.STOPED:
             print("Micro stopped! Halt executed")
+        else:
+            # - Fetch the current instruction
+            ir = self._mem[self._PC]
+
+            # - Print the current instruction
+            print("Next: [{:03X}] {}".format(self._PC, self.code2asm(ir)))
 
     def load(self, prog):
         """Load a program into the memory. The program consist of a list of consecutive
            instructions"""
 
         l = len(prog)
-        s.mem[0:l] = prog
+        s._mem[0:l] = prog
 
-    def print_mem(self, nblocks=32):
+    def mem(self, nblocks=32):
         """Print the memory
            nblocks = number of blocks (of 16 words) to print"""
         print("       0   1   2   3   4   5   6   7   8   9   A   B   C   D   E   F")
         for block in range(nblocks):
             linecode = ""
-            for pos in self.mem[(block * 16):(block*16 + 16)]:
+            for pos in self._mem[(block * 16):(block*16 + 16)]:
                 linecode += "{:03X} ".format(pos)
 
             print("[{:03X}] {}".format(block*16, linecode))
 
+    def _execute(self, opcode, addr, xopcode):
+        """Execute the give instruction"""
+
+        if opcode == self.OP_ST:
+            self._mem[addr] = self._A
+
+        elif opcode == self.OP_LD:
+            self._A = self._mem[addr]
+
+        elif opcode == self.OP_ADD:
+            self._A += self._mem[addr]
+
+        elif opcode == self.OP_CLR:
+            self._A = 0
+
+        elif opcode == self.OP_DEC:
+            self._A -= 1
+
+        elif opcode == self.OP_BR:
+            self._PC = addr
+
+        elif opcode == self.OP_BZ:
+            if self._Z == 1:
+                self._PC = addr
+
+        elif opcode == self.OP_HALT:
+            """Check the extended instructions"""
+            print("HALT. xopcode: {}".format(xopcode))
+            if xopcode == self.XOP_HALT:
+                self.state = self.STOPED
+
     def step(self):
+        """Simulate the next instruction"""
 
         if self.state == self.STOPED:
             print("Micro stopped! Halt executed")
@@ -77,10 +115,10 @@ class simplez(object):
             self.state = self.RUNNING
 
         # - Fetch the current instruction
-        ir = self.mem[self._PC]
+        ir = self._mem[self._PC]
 
         # - Print the current instruction
-        print("[{:03X}] {}".format(self._PC, self.code2asm(ir)))
+        print("Executing: [{:03X}] {}".format(self._PC, self.code2asm(ir)))
 
         # - Point to the next instruction
         self._PC += 1
@@ -88,28 +126,11 @@ class simplez(object):
         # - Decode the instruction
         opcode, addr = self.decode(ir)
 
-        # - Execute!
-        if opcode == self.OP_ST:
-            self.mem[addr] = self._A
+        # - Decode de extended instructions
+        xopcode = ir >> 8
 
-        elif opcode == self.OP_LD:
-            self._A = self.mem[addr]
-
-        elif opcode == self.OP_ADD:
-            self._A += self.mem[addr]
-
-        elif opcode == self.OP_CLR:
-            self._A = 0
-
-        elif opcode == self.OP_DEC:
-            self._A -= 1
-
-        elif opcode == self.OP_HALT:
-            """Check the extended instructions"""
-            xopcode = ir >> 8
-            print("HALT. xopcode: {}".format(xopcode))
-            if xopcode == self.XOP_HALT:
-                self.state = self.STOPED
+        # - Execute
+        self._execute(opcode, addr, xopcode)
 
         # - Update the Z flag
         if self._A == 0:
@@ -132,7 +153,7 @@ class simplez(object):
 
         # - Add the content of the memory (as a comment)
         if nemonic in ["ST", "LD", "ADD"]:
-            asm += ";  [{:03X}]={:03X}".format(addr, self.mem[addr])
+            asm += ";  [{:03X}]={:03X}".format(addr, self._mem[addr])
 
         return asm
 
@@ -145,4 +166,4 @@ if __name__ == "__main__":
     # - Load a simple program
     s.load(Test_progs.SIMPLEZ1)
 
-    s.step()
+    # - s.step()
