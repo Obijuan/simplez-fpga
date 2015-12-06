aae3f1be310927c28a90f77bbce730fb11dda5f5
obijuan <juan@iearobotics.com>
2015-12-06 12:25:11 +0100

    Removed the NOP instruction from all the t*.asm programs

--- verilog/asm/t09.asm
+++ verilog/asm/t09.asm
@@ -2,19 +2,6 @@
 ;-- t09.asm: Prueba 1 de CLR
 
 
-;---------------------------------------------------------------------------------
-; NOTA: En yosys/icestorm hay un bug que hace que el contenido de la
-; posicion 0 de la RAM se ponga a 0 esporádicamente al arrancar
-; Por ello la primera instruccion se debe ignorar. El programa debe empezar
-; en la direccion 1
-;
-; $ yosys -V
-; Yosys 0.5+397 (git sha1 c86fbae, clang 3.6.2-1 -fPIC -Os)
-;---------------------------------------------------------------------------------
-NOP  DATA 0    ;-- Ignorar esta intruccion
-
-;-- El programa comienza aqui
-
        ld /val1   ; Inicializar acumulador
        WAIT
        CLR
--- verilog/asm/t11.asm
+++ verilog/asm/t11.asm
@@ -2,20 +2,6 @@
 ;-- t11.asm: Prueba 1 de BZ
 
 
-;---------------------------------------------------------------------------------
-; NOTA: En yosys/icestorm hay un bug que hace que el contenido de la
-; posicion 0 de la RAM se ponga a 0 esporádicamente al arrancar
-; Por ello la primera instruccion se debe ignorar. El programa debe empezar
-; en la direccion 1
-;
-; $ yosys -V
-; Yosys 0.5+397 (git sha1 c86fbae, clang 3.6.2-1 -fPIC -Os)
-;---------------------------------------------------------------------------------
-NOP  DATA 0    ;-- Ignorar esta intruccion
-
-;-- El programa comienza aqui
-
-
        ld /comp     ; Valor a comparar
        bz /iscero
 
