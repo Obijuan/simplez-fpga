f7f4a32735c46a1a9ab4626d0d1357e7b86661b2
obijuan <juan@iearobotics.com>
2015-12-06 11:18:22 +0100

    Tested program t01 - t04.asm with the new yosys 0.5+408. They work without adding the NOP operation

--- verilog/asm/t01.asm
+++ verilog/asm/t01.asm
@@ -1,17 +1,6 @@
 ;-- Programas de prueba para Simplez
 ;-- t01.asm: HALT
 
-;---------------------------------------------------------------------------------
-; NOTA: En yosys/icestorm hay un bug que hace que el contenido de la
-; posicion 0 de la RAM se ponga a 0 esporádicamente al arrancar
-; Por ello la primera instruccion se debe ignorar. El programa debe empezar
-; en la direccion 1
-;
-; $ yosys -V
-; Yosys 0.5+397 (git sha1 c86fbae, clang 3.6.2-1 -fPIC -Os)
-;---------------------------------------------------------------------------------
-NOP  DATA 0    ;-- Ignorar esta intruccion
-
 ;--- Comienzo del programa
 
 end  HALT  ;-- Terminar
