caa649368aec4c0eae9d65907b51dab716e49672
obijuan <juan@iearobotics.com>
2015-12-10 10:05:12 +0100

    Example t15.asm: comments added

--- verilog/asm/t15.asm
+++ verilog/asm/t15.asm
@@ -3,23 +3,27 @@
 ;--   estado de la pantalla
 
 
-       ld /val1
-       WAIT
+       ld /val1        ;-- Encender todos los leds
+       WAIT            ;-- Y esperar a que todo esté correctamente incializado
 
+;-------- Envio de "H"
 wait1
-       ld /TXSTATUS
-       BZ /wait1
+       ld /TXSTATUS    ;-- Leer el registro de estado de la pantalla
+       BZ /wait1       ;-- Esperar a que se ponga a 1
 
-       ld /car_H
-       st /TXDATA
+       ld /car_H       ;-- Enviar el caracter "H"
+       st /TXDATA      ;-- Escribiendo en el registro de datos
 
+
+;------- Envio de "O"
 wait2
-       ld /TXSTATUS
-       BZ /wait2
+       ld /TXSTATUS    ;-- Lo mismo que en el bloque anterior
+       BZ /wait2       ;-- Pero con el siguiente caracter
 
        ld /car_O
        st /TXDATA
 
+;------- Envio de "L"
 wait3
        ld /TXSTATUS
        BZ /wait3
@@ -27,6 +31,7 @@ wait3
        ld /car_L
        st /TXDATA
 
+;------- Envio de "A"
 wait4
        ld /TXSTATUS
        BZ /wait4
@@ -34,6 +39,7 @@ wait4
        ld /car_A
        st /TXDATA
 
+;------- Envio de " "
 wait5
        ld /TXSTATUS
        BZ /wait5
@@ -41,6 +47,7 @@ wait5
        ld /car_sp
        st /TXDATA
 
+       ;-- Terminar el programa
        HALT
 
 val1    DATA H'F
@@ -51,7 +58,8 @@ car_A   DATA  h'41   ;-- "A"
 car_sp  DATA  h'20   ;-- " "
 
 
+;-- Direcciones de acceso a la pantalla
 
           ORG 508
-TXSTATUS  DATA    0  ;-- 508
-TXDATA    DATA    0  ;-- 509
+TXSTATUS  DATA    0  ;-- 508:  Registro de estado
+TXDATA    DATA    0  ;-- 509:  Registro de datos
