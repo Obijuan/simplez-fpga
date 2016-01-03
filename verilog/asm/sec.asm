da71646068b2812263a17243dce4a46ced9c4881
obijuan <juan@iearobotics.com>
2016-01-03 17:35:52 +0100

    [Simplez][Doc] sec.asm program for showing two states on the leds

---
+++ verilog/asm/sec.asm
@@ -0,0 +1,23 @@
+;-- Programas de ejemplo para Simplez
+;-- leds_on.asm: Encender todos los leds
+
+loop
+    LD /val1   ;-- Sacar valor 1 por los leds
+    ST /LEDS
+    WAIT       ; Pausa
+    LD /val2   ;-- Sacar valor 2 por los leds
+    ST /LEDS
+    WAIT
+    BR /loop   ;-- Repetir
+
+
+;-- Datos
+val1  DATA H'03   ;-- Valor 1 de la secuencia
+val2  DATA H'0C   ;-- Valor 2 de la secuencia
+
+
+;------ PERIFERICO: puerto de leds ------------------
+
+          ORG 507
+
+LEDS      DATA    0  ;-- Todo lo escrito aqui se saca por los leds
