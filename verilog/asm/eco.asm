47cf378c0c909afdf93d87fea88711f86aac2073
obijuan <juan@iearobotics.com>
2016-01-03 17:21:21 +0100

    [SIMPLEZ][DOC] Example program eco.asm and leds_on.asm

---
+++ verilog/asm/eco.asm
@@ -0,0 +1,57 @@
+;-- Programas de ejemplo para Simplez
+;-- eco.asm: Se hace eco de lo recibido por el puerto serie y ademas se saca por los leds
+
+;-- Lanzar una rafaga por los leds, para indicar que arranca el programa
+        LD /cval1
+        ST /LEDS
+        WAIT
+        CLR
+        ST /LEDS
+
+;-- Bucle principal: Incrementar contador cada vez que se recibe un caracter
+main
+
+       ;-- Esperar a que llegue un caracter
+       LD /RXSTATUS
+       BZ /main
+
+       ;-- Leer caracter
+       LD /RXDATA
+
+       ;-- Sacarlo por los leds
+       ST /LEDS
+
+       ;-- Alcacenar caracter recibido
+       ST /car
+
+       ;-- Enviarlo de vuelta
+
+txloop ;-- Esperar a que pantalla lista
+       LD /TXSTATUS
+       BZ /txloop
+
+       ;-- Sacarlo por pantalla
+       LD /car
+       ST /TXDATA
+
+       BR /main
+
+;-- Variables y constantes
+cval1  DATA  H'0F   ;-- Valor constante
+car    DATA  0      ;-- Caracter recibido
+
+
+;------ PERIFERICOS ------------------
+
+          ORG 507
+;-- LEDS
+LEDS      DATA    0  ;-- 507: Escritura en leds
+
+
+;--- PANTALLA
+TXSTATUS  DATA    0  ;-- 508:  Registro de estado
+TXDATA    DATA    0  ;-- 509:  Registro de datos
+
+;-- Direcciones de acceso al teclado
+RXSTATUS  DATA    0  ;-- 510:  Registro de estado
+RXDATA    DATA    0  ;-- 511:  Registro de datos
---
+++ verilog/asm/leds_on.asm
@@ -0,0 +1,16 @@
+;-- Programas de ejemplo para Simplez
+;-- leds_on.asm: Encender todos los leds
+
+    LD /val    ; Cargar en A valor a enviar al puerto de los leds
+    ST /LEDS   ; Escribir valor en el puerto de leds
+    HALT       ; Terminar
+
+;-- Datos
+val  DATA H'0F   ;-- Valor a sacar por los leds
+
+
+;------ PERIFERICO: puerto de leds ------------------
+
+          ORG 507
+
+LEDS      DATA    0  ;-- Todo lo escrito aqui se saca por los leds
