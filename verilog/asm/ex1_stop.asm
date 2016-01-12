b163f8b0ec65af299299d48bdc6a799e7f1c0d9a
obijuan <juan@iearobotics.com>
2016-01-12 08:32:45 +0100

    [sasm] t01.asm renamed as ex1_stop.asm. Tested

---
+++ verilog/asm/ex1_stop.asm
@@ -0,0 +1,9 @@
+;-- Programas de prueba para Simplez
+;-- Ejemplo 1: Instruction HALT
+;-- Se ejecuta la instruccion HALT y el programa termina
+;-- Se encendera el led verde de la icestick, indicando que se ha detenido
+
+;--- Comienzo del programa
+
+     HALT  ;-- Terminar
+     end
--- verilog/asm/t01.asm
+++
@@ -1,8 +0,0 @@
-;-- Programas de prueba para Simplez
-;-- t01.asm: HALT
-
-;--- Comienzo del programa
-
-     HALT  ;-- Terminar
-     end
-     
