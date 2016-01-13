0be29367a8990e988ceb0c96bef7c32fb42a9726
obijuan <juan@iearobotics.com>
2016-01-13 10:38:56 +0100

    [sasm] Example 11: writing the word HOLA on the screen (using the screen status register

--- verilog/asm/ex11_pant2.asm
+++ verilog/asm/ex11_pant2.asm
@@ -1,14 +1,16 @@
 ;-- Programas de prueba para Simplez
-;-- t15.asm: Prueba de escritura en la pantalla usando el registro de
-;--   estado de la pantalla
+;-- Ejemplo 11: Escritura en pantalla. Se escribe la palabra "HOLA "
+;-- Se usa el registro de estado para saber cuando esta lista la pantalla
+;-- para recebir nuevos caracteres
 
+       WAIT         ;-- Inicio: esperar 200ms
 
        ld /val1        ;-- Encender todos los leds
+       st /LEDS
        WAIT            ;-- Y esperar a que todo esté correctamente incializado
 
 ;-------- Envio de "H"
-wait1
-       ld /TXSTATUS    ;-- Leer el registro de estado de la pantalla
+wait1  ld /TXSTATUS    ;-- Leer el registro de estado de la pantalla
        BZ /wait1       ;-- Esperar a que se ponga a 1
 
        ld /car_H       ;-- Enviar el caracter "H"
@@ -16,32 +18,28 @@ wait1
 
 
 ;------- Envio de "O"
-wait2
-       ld /TXSTATUS    ;-- Lo mismo que en el bloque anterior
+wait2  ld /TXSTATUS    ;-- Lo mismo que en el bloque anterior
        BZ /wait2       ;-- Pero con el siguiente caracter
 
        ld /car_O
        st /TXDATA
 
 ;------- Envio de "L"
-wait3
-       ld /TXSTATUS
+wait3  ld /TXSTATUS
        BZ /wait3
 
        ld /car_L
        st /TXDATA
 
 ;------- Envio de "A"
-wait4
-       ld /TXSTATUS
+wait4 ld /TXSTATUS
        BZ /wait4
 
        ld /car_A
        st /TXDATA
 
 ;------- Envio de " "
-wait5
-       ld /TXSTATUS
+wait5  ld /TXSTATUS
        BZ /wait5
 
        ld /car_sp
@@ -51,15 +49,21 @@ wait5
        HALT
 
 val1    DATA H'F
-car_H   DATA  h'48   ;-- "H"
-car_O   DATA  h'4F   ;-- "O"
-car_L   DATA  h'4C   ;-- "L"
-car_A   DATA  h'41   ;-- "A"
-car_sp  DATA  h'20   ;-- " "
 
+car_H   DATA  "H"
+car_O   DATA  "O"
+car_L   DATA  "L"
+car_A   DATA  "A"
+car_sp  DATA  " "
 
-;-- Direcciones de acceso a la pantalla
+;----------------- PERIFERICOS
 
-          ORG 508
+          ORG 507
+;-- LEDS
+LEDS      DATA    0  ;-- 507: Escritura en leds
+
+;-- Pantalla
 TXSTATUS  DATA    0  ;-- 508:  Registro de estado
 TXDATA    DATA    0  ;-- 509:  Registro de datos
+
+end
