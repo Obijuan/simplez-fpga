9d1b922bcb29f847ec1148afe598ffeb05aa023f
obijuan <juan@iearobotics.com>
2016-01-14 10:44:26 +0100

    [sboot] .list files can be given by parameters

--- verilog/sboot.py
+++ verilog/sboot.py
@@ -1,7 +1,25 @@
 #!/usr/bin/python3
+# --------------------------------------------------------------------------------------
+# -  Cargador de programas en SIMPLEZ
+# --  (C) BQ Enero 2016. Written by Juan Gonzalez (obijuan)
+# --  Python 3
+# --------------------------------------------------------------------------------------
+# - La FPGA tiene que estar configurada con SIMPLEZ con el programa boot.asm grabado
+# - (es un bootloader).
+# - sboot.py se comunica con el bootloader
+# - Al arrancar SIMPLEZ se ejecuta el bootloader. El protocolo es el siguiente:
+# -
+# -  Bootloader: Envía el caracter BREADY para indicar que esta listo
+# -  sboot envía primero el tamaño del fichero a cargar, en palabras
+# -  Cada palabra se envía como 2 bytes, primero el de mayor peso y luego el de menor
+# -  A continuación sboot comienza a enviar todas las palabras que componen el programa
+# -  Cuando se han enviado todas, el bootloader le pasa el control al nuevo programa
+# -  cargado
+# -----------------------------------------------------------------------------------------
 
 import serial
 import time
+from ssim import simplez
 
 # - Example programs. For testing
 LEDS = [0x303, 0x1FB, 0xE00, 0x00E]
@@ -10,6 +28,9 @@ SEC2 = [0x307, 0x1FB, 0xF00, 0x308, 0x1FB, 0xF00, 0x700, 0x00C, 0x003]
 
 # -- Byte that is received when the bootloader is ready
 BREADY = b'B'
+
+# -- Initial address were the programs are loaded
+INITIAL_ADDR = 0x100
 
 
 # -- Download the program
@@ -32,8 +53,46 @@ def download(ser, prog):
         ser.write(instb)
 
 
+def parse_arguments():
+    """Parse the arguments"""
+
+    import argparse
+    description = """SIMPLEZ Loader.  Load programs into the simplez RAM memory"""
+
+    # -- Add the description
+    parser = argparse.ArgumentParser(description=description)
+
+    # -- Add the assembler input file
+    parser.add_argument("inputfile", help="Simplez machine code file (.list)")
+
+    # -- Parse the anguments
+    args = parser.parse_args()
+
+    return args.inputfile
+
+
 # -- Main program
 if __name__ == '__main__':
+
+    # -- Process the arguments
+    input_file = parse_arguments()
+
+    # - Create the virtual simplez processor
+    s = simplez()
+
+    print("File: {}".format(input_file))
+
+    # -- Load the machine code to simulate
+    s.load_mcode_file(input_file)
+
+    end_addr = s._load_addr
+    print("Final address: h'{:03X}".format(end_addr))
+
+    tam = s._load_addr - INITIAL_ADDR
+    print("Size: {} words".format(tam))
+
+    prog = s._mem[INITIAL_ADDR: end_addr]
+
     ser = serial.Serial('/dev/ttyUSB1', baudrate=115200, timeout=0.5)
 
     # -- Reset Simplez
@@ -49,6 +108,6 @@ if __name__ == '__main__':
 
     # download(ser, LEDS)
     # download(ser, SEC2)
-    download(ser, SEC1)
+    download(ser, prog)
 
     print("EXECUTING!!!")
