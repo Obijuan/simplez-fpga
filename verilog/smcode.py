b16fa4ef29081ac9cbf39e614f6b8e3d08f20264
obijuan <juan@iearobotics.com>
2016-01-17 21:56:22 +0100

    [smcode] Simplez machine code module: parsing the .list files. Initial draft

---
+++ verilog/smcode.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+
+import ply.lex as lex
+
+tokens = (
+   'COMMENT',
+   'ADDR',      # -- Value: addr (in hexa)
+   'DATA',      # -- Value: Data (in hexa)
+)
+
+t_ignore = ' \t\r\f\v'
+
+
+# - Comments are ignored
+def t_COMMENT(t):
+    r'//[^\n]*'
+
+
+def t_DATA(t):
+    r'[0-9a-fA-F]+'
+    t.value = int(t.value, 16)
+    return t
+
+
+def t_ADDR(t):
+    r'@[0-9a-fA-F]+'
+    t.value = int(t.value[1:], 16)
+    return t
+
+
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+
+
+# Error handling rule
+# def t_error(t):
+#    print("Illegal character '%s'" % t.value[0])
+#    t.lexer.skip(1)
+
+# Build the lexer
+lexer = lex.lex()
+
+data = '''
+//-- Hola
+@40  //-- Comienzo
+gg
+10 20   //-- Una inst
+30   //-- Otra...
+caca
+'''
+
+# Give the lexer some input
+lexer.input(data)
+
+# Tokenize
+while True:
+    tok = lexer.token()
+    if not tok:
+        break      # No more input
+    print(tok)
