59fa2197f4d9b8e147bdea694ab3405b9ab1965d
obijuan <juan@iearobotics.com>
2017-12-13 15:38:53 +0100

    Removed the an error in the package description that prevented it from being uploaded to pypi

--- toolchain-simplez/setup.py
+++ toolchain-simplez/setup.py
@@ -6,22 +6,13 @@ import toolchain_simplez
 
 setup(name='toolchain-simplez',
       version=toolchain_simplez.__version__,
-      description=toolchain_simplez.__doc__,
+      description="Toolchain for the Simplez educational processor",
       author=toolchain_simplez.__author__,
       author_email=toolchain_simplez.__email__,
       url='https://github.com/Obijuan/simplez-fpga',
       download_url='https://pypi.python.org/pypi/toolchain-simplez',
       license=toolchain_simplez.__license__,
-      py_modules=['toolchain_simplez', 'sasm', 'sreset', 'sboot', 'vmem',
-                  'consola_io', 'ssim'],
-      entry_points={'console_scripts': ['sasm=sasm:main',
-                                        'sreset=sreset:main',
-                                        'sboot=sboot:main']},
+      py_modules=['toolchain_simplez', 'sasm', 'sreset', 'sboot', 'vmem','consola_io', 'ssim'],
+      entry_points={'console_scripts': ['sasm=sasm:main','sreset=sreset:main','sboot=sboot:main']},
       install_requires=['pyserial', 'ply'],
-      classifiers=[
-          'Development Status :: 1 - Planning',
-          'Environment :: Console',
-          'Intended Audience :: Developers',
-          'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
-          'Programming Language :: Python :: 3.4'
-      ])
+      classifiers=['Development Status :: 1 - Planning', 'Environment :: Console', 'Intended Audience :: Developers', 'License :: OSI Approved :: GNU General Public License v2 (GPLv2)', 'Programming Language :: Python :: 3.4'])
