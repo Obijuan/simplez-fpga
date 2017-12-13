a07ea9bb8499a558d4e50d0f1c77af2d166c8773
obijuan <juan@iearobotics.com>
2017-12-13 16:04:12 +0100

    simplez toolchain updated to 0.15

--- toolchain-simplez/toolchain_simplez.py
+++ toolchain-simplez/toolchain_simplez.py
@@ -4,15 +4,10 @@
 
 """
 Toolchain-simplez are a set of tools for working with the education processor
-Simplez. The included tools are:
-
-* sasm: Simplez Assembler
-* ssim: Simplez simulator
-* sreset: Simplez Reset
-* sboot: Simplez loader (a bootloader is needed)
+Simplez. The included tools are: sasm (Simplez Assembler), ssim (Simplez simulator), sreset (Simplez Reset) and sboot (Simplez loader)
 """
 
 __author__ = 'Juan Gonzalez-Gomez (obijuan)'
 __email__ = 'obijuan.cube@gmail.com'
 __license__ = 'GPLv2'
-__version__ = '0.0.14'
+__version__ = '0.0.15'
